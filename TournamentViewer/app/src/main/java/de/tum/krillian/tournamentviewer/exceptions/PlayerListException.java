1aec09f333a8554b9ad1f3f71b239fd0e4cf2869
0Chris0 <ziegner.christian@gmail.com>
2016-08-25 22:46:34 +0200

    added exception

---
+++ TournamentViewer/app/src/main/java/de/tum/krillian/tournamentviewer/exceptions/PlayerListException.java
@@ -0,0 +1,13 @@
+package de.tum.krillian.tournamentviewer.exceptions;
+
+/**
+ * Created by Christian on 25.08.2016.
+ */
+public class PlayerListException extends Exception {
+
+    public PlayerListException(){ }
+
+    public PlayerListException(String message){
+        super(message);
+    }
+}
