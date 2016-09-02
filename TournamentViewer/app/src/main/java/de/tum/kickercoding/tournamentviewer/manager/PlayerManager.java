5d7a81175666accb8283413d51bb9bf1ac5ab02b
0Chris0 <ziegner.christian@gmail.com>
2016-09-02 18:47:57 +0200

    updated PlayerManager

--- TournamentViewer/app/src/main/java/de/tum/kickercoding/tournamentviewer/manager/PlayerManager.java
+++ TournamentViewer/app/src/main/java/de/tum/kickercoding/tournamentviewer/manager/PlayerManager.java
@@ -1,26 +1,46 @@
 package de.tum.kickercoding.tournamentviewer.manager;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import de.tum.kickercoding.tournamentviewer.entities.Game;
 import de.tum.kickercoding.tournamentviewer.entities.Player;
-import de.tum.kickercoding.tournamentviewer.exceptions.PlayerMangerException;
+import de.tum.kickercoding.tournamentviewer.exceptions.PlayerManagerException;
+import de.tum.kickercoding.tournamentviewer.exceptions.PreferenceFileException;
 
 
 // TODO: add comments to methods/class
 class PlayerManager {
 
-    private static List<Player> players = new ArrayList<Player>();
+    private static PlayerManager instance = null;
+    private PreferenceFileManager preferenceFileManager = null;
+    //private Context context = null;
+    private List<Player> players = new ArrayList<Player>();
 
-    private PlayerManager(){}
+    private PlayerManager(){
+        preferenceFileManager = PreferenceFileManager.getInstance();
+        players = preferenceFileManager.getPlayerList();
+    }
+
+    /**
+     * Get the instance of the singleton.
+     * @return The instance of the singleton.
+     */
+    static PlayerManager getInstance() {
+        if (instance == null) {
+            instance = new PlayerManager();
+        }
+        return instance;
+    }
 
     /**
      * Add player to player list.
      * @param name The name of the player to add.
      * @throws PlayerManagerException
      */
-    public static void addPlayer(String name) throws PlayerManagerException {
+    void addPlayer(String name) throws PlayerManagerException {
         if (name.contains("|")) {
             throw new PlayerManagerException("Invalid character: | (pipe symbol)");
         }
@@ -29,11 +49,15 @@ class PlayerManager {
         if(players.contains(newPlayer)){
             throw new PlayerManagerException(String.format("Player %s already exists", name));
         }
-        // TODO: check for | in name
         players.add(newPlayer);
     }
 
-    public static void removePlayer(String name) throws PlayerManagerException {
+    /**
+     * Remove player from player list.
+     * @param name The name of the player to remove.
+     * @throws PlayerManagerException
+     */
+    void removePlayer(String name) throws PlayerManagerException {
         for(Player p : players) {
             if(p.getName().equals(name)) {
                 players.remove(p);
@@ -43,16 +67,24 @@ class PlayerManager {
         throw new PlayerManagerException("Failed to remove player");
     }
 
-    public static int getNumberOfPlayers() {
+    /**
+     * Get the number of currently registered players.
+     * @return Number of players.
+     */
+    int getNumberOfPlayers() {
         return players.size();
     }
 
-    public static void commitGameResult(Game game){
-        // TODO
+    /**
+     * Commit results of a game to the player list
+     * @param game
+     */
+    void commitGameResult(Game game){
+        // TODO: implement
     }
 
     // Used for testing only
-    public static void clearPlayerList() {
+    public void clearPlayerList() {
         players.clear();
     }
 }
