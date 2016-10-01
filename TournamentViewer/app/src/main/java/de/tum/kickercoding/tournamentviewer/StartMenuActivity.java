35b64ca889b83e701fb38dc4045bd8544bc3496a
Krillian111 <krillian@gmx.de>
2016-10-01 20:09:43 +0200

    added buttons and respective methods to call to load last tournament StartMenuActivity: renamed from StartActivity

--- TournamentViewer/app/src/main/java/de/tum/kickercoding/tournamentviewer/StartMenuActivity.java
+++ TournamentViewer/app/src/main/java/de/tum/kickercoding/tournamentviewer/StartMenuActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import de.tum.kickercoding.tournamentviewer.exceptions.AppManagerException;
 import de.tum.kickercoding.tournamentviewer.manager.AppManager;
 import de.tum.kickercoding.tournamentviewer.setup.monsterdyp.MonsterDypBasicSetupActivity;
+import de.tum.kickercoding.tournamentviewer.tournament.monsterdyp.MonsterDypTournamentActivity;
 import de.tum.kickercoding.tournamentviewer.util.TournamentMode;
 
 /**
@@ -15,20 +17,33 @@ import de.tum.kickercoding.tournamentviewer.util.TournamentMode;
  */
 // TODO: add possibility to load pending tournament (click on monsterdyp calls initialization of TournamentManager
 // which resets the Tournament
-public class StartActivity extends AppCompatActivity {
+public class StartMenuActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_start);
+		setContentView(R.layout.activity_start_menu);
 	}
 
 	/**
 	 * Called when the user clicks the MonsterDYP button
 	 */
 	public void monsterDypSetup(View view) {
-		AppManager.getInstance().initializeTournamentManager(TournamentMode.MONSTERDYP);
+		AppManager.getInstance().setTournamentMode(TournamentMode.MONSTERDYP);
 		Intent intent = new Intent(this, MonsterDypBasicSetupActivity.class);
 		startActivity(intent);
+	}
+
+	/**
+	 * Called when the user clicks the load tournament button
+	 */
+	public void loadLastTournament(View view) {
+		try {
+			AppManager.getInstance().loadTournament();
+			Intent intent = new Intent(this, MonsterDypTournamentActivity.class);
+			startActivity(intent);
+		} catch (AppManagerException e) {
+			AppManager.getInstance().displayError(this, e.getMessage());
+		}
 	}
 }
